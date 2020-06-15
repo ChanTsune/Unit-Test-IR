76a6d3448468d15f94b2762667f932df55428350
ChanTsune <s1f101702431@iniad.org>
2020-06-15 22:02:53 +0900

    Update __init__.py

--- UTIR/loader/__init__.py
+++ UTIR/loader/__init__.py
@@ -3,10 +3,6 @@ import yaml
 
 class YamlLoader:
 
-    def loadf(self, file):
-        with open(file, 'r') as f:
+    def loadf(self, path):
+        with open(path, 'r') as f:
             return yaml.load(f, yaml.SafeLoader)
-
-    def dumpf(self, file, obj):
-        with open(file, 'w') as f:
-            yaml.dump(obj, f)
