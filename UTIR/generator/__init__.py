9e2cc55dd35bb0c77022698d5446b1a69a23f3fd
ChanTsune <s1f101702431@iniad.org>
2020-06-16 16:17:40 +0900

    Add PyASTToCodeGenerator

--- UTIR/generator/__init__.py
+++ UTIR/generator/__init__.py
@@ -1,7 +1,8 @@
 import ast
 import astor
-from . import ast as ir_ast
 
 
-def generate_python_code_from_utir_ast(ir_ast):
-    return astor.to_source(ir_ast.to_py_ast())
+class PyASTToCodeGenerator:
+
+    def generate(self, py_ast):
+        return astor.to_source(py_ast)
