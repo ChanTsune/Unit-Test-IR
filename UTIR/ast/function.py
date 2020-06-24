e5dbacd946af61ed0c1bb207746a851c4792d098
taiki tsunekawa <s1f101702431@iniad.org>
2020-06-24 17:45:33 +0900

    Add FunctionDef

--- UTIR/ast/function.py
+++ UTIR/ast/function.py
@@ -1,6 +1,29 @@
 from .base import Expression
 
 
+class FunctionDef(Expression):
+    """UTIR FunctionDef Expression"""
+
+    def __init__(self, name, args, kwargs, body):
+        self.name = name
+        self.args = args
+        self.kwargs = kwargs
+        self.body = body
+
+    def serialize(self):
+        return {
+            'FunctionDef': {
+                'Name': self.name,
+                'Args': [i.serialize() for i in self.args],
+                'KwArgs': {k: v.serialize() for k, v in self.kwargs.items()},
+                'Body': [i.serialize() for i in self.body],
+            }
+        }
+
+    def _dump(self):
+        return f'name={self.name},args={[i.dump() for i in self.args]},kwargs={dict({k: v.dump() for k, v in self.kwargs.items()})},body={[i.dump() for i in self.body]}'
+
+
 class FunctionCall(Expression):
     """UTIR FunctionCall Expression"""
 
