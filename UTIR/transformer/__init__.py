b4a380ada1ef8a05fb166135d74193388791ea52
taiki tsunekawa <s1f101702431@iniad.org>
2020-06-24 22:32:45 +0900

    Create __init__.py

---
+++ UTIR/transformer/__init__.py
@@ -0,0 +1,30 @@
+from UTIR.ast import AST
+from UTIR.visitor import NodeVisitor, iter_fields
+
+class NodeTransformer(NodeVisitor):
+    """NodeTransformer class"""
+
+    def transform(self, node):
+        return self.visit(node)
+
+    def generic_visit(self, node):
+        for field, old_value in iter_fields(node):
+            if isinstance(old_value, list):
+                new_values = []
+                for value in old_value:
+                    if isinstance(value, AST):
+                        value = self.visit(value)
+                        if value is None:
+                            continue
+                        elif not isinstance(value, AST):
+                            new_values.extend(value)
+                            continue
+                    new_values.append(value)
+                old_value[:] = new_values
+            elif isinstance(old_value, AST):
+                new_node = self.visit(old_value)
+                if new_node is None:
+                    delattr(node, field)
+                else:
+                    setattr(node, field, new_node)
+        return node
