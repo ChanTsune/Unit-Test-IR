b58ae178f3a6e3bf896fe128e74bbfe2c62c5d8e
taiki tsunekawa <41658782+ChanTsune@users.noreply.github.com>
2020-06-28 16:24:18 +0900

    Update function.py

--- UTIR/ast/function.py
+++ UTIR/ast/function.py
@@ -4,24 +4,28 @@ from .base import Expression
 class FunctionDef(Expression):
     """UTIR FunctionDef Expression"""
 
-    def __init__(self, name, args, kwargs, body):
+    def __init__(self, name, kind, args, kwargs, body, return_type):
         self.name = name
+        self.kind = kind
         self.args = args
         self.kwargs = kwargs
         self.body = body
+        self.return_type = return_type
 
     def serialize(self):
         return {
             'FunctionDef': {
                 'Name': self.name,
+                'Kind': self.kind,
                 'Args': [i.serialize() for i in self.args],
                 'KwArgs': {k: v.serialize() for k, v in self.kwargs.items()},
                 'Body': [i.serialize() for i in self.body],
+                'ReturnType': self.return_type,
             }
         }
 
     def _dump(self):
-        return f'name={self.name},args={[i.dump() for i in self.args]},kwargs={dict({k: v.dump() for k, v in self.kwargs.items()})},body={[i.dump() for i in self.body]}'
+        return f'name={self.name},args={[i.dump() for i in self.args]},kwargs={dict({k: v.dump() for k, v in self.kwargs.items()})},body={[i.dump() for i in self.body]},return_type={self.return_type}'
 
 
 class Call(Expression):
