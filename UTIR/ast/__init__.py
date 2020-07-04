0b8617390e41892ef942c57aa5572125fc97944c
taiki tsunekawa <41658782+ChanTsune@users.noreply.github.com>
2020-07-04 21:32:28 +0900

    Add nodes.py

--- UTIR/ast/__init__.py
+++ UTIR/ast/__init__.py
@@ -1,6 +1,2 @@
-from .base import Expression, AssignExpression, Name, AST, Attribute, File
-from .array import Array
-from .function import Call, FunctionDef, ArgumentDef, Return
-from .struct import ClassDef
-from .suite import TestSuite, TestCase, TestProject, Assert
-from .value import Value
+from .base import AST, Node
+from .nodes import *
