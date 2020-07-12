486a1cf9f84c2a8c055b7d4aab2a854f115f5f40
taiki tsunekawa <41658782+ChanTsune@users.noreply.github.com>
2020-07-12 11:00:39 +0900

    Add example

---
+++ example/transformers.py
@@ -0,0 +1,26 @@
+import ast
+from ast import NodeTransformer
+
+
+class MyTransformer(NodeTransformer):
+
+    def visit_Call(self, node):
+        print(node)
+        func = node.func
+        if isinstance(func, ast.Attribute):
+            if func.attr == 'checkequal':
+                func.attr = 'assertEqual'
+                args = node.args
+                keywords = node.keywords
+                method = ast.Attribute(value=args[1], attr=args[2].s)
+
+                call = ast.Call(func=method, args=args[3:], keywords=keywords)
+                return ast.Call(func=func,
+                                args=[args[0], call],
+                                keywords=[])
+        return node
+
+    def visit_FunctionDef(self, node):
+        if node.name == 'checkequal':
+            return None
+        return self.generic_visit(node)
--- main.py
+++ main.py
@@ -33,43 +33,5 @@ def _main(argv):
     ir2code.convert(argv[2], '')
 
 
-def generate_ast_main(argv):
-    from UTIR.reader import SourceReader
-    from UTIR.converter import PyAST2IRASTConverter, IRAST2PyASTConverter
-    from UTIR.loader import YamlLoader
-    from UTIR.deserializer import ASTDeserializer
-    # ソースコードの読み込み
-    reader = SourceReader()
-    python_ast = reader.readf(argv[1])
-    # from ast import dump
-    # print(dump(python_ast))
-    # return
-
-    # ASTを中間表現ASTに変換
-    converter = PyAST2IRASTConverter()
-    ir_ast = converter.convert(python_ast)
-
-    visitor = SampleVisitor()
-    visitor.visit(ir_ast)
-    # 中間表現IRのシリアライズ
-    test_suite = ir_ast
-    ast_serializer = serializer.ASTSerializer()
-    object = ast_serializer.serialize(test_suite)
-    # 中間表現ASTの書き出し
-    object_dumper = dumper.YamlDumper()
-    object_dumper.dumpf(argv[2], object)
-
-    # 中間表現ファイルの読み込み
-    object_loader = YamlLoader()
-    object = object_loader.loadf(argv[2])
-    ast_deserializer = ASTDeserializer()
-    loaded_ir_ast = ast_deserializer.deserialize(object)
-    ir_to_py_comverter = IRAST2PyASTConverter()
-    target_ast = ir_to_py_comverter.convert(loaded_ir_ast)
-    ast_generator = generator.PyASTToCodeGenerator()
-    code = ast_generator.generate(target_ast)
-    print(code)
-
-
 if __name__ == "__main__":
     main(sys.argv)
