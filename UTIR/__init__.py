6d7a250171f68799f3abb5f2fb3f44300dd16220
taiki tsunekawa <41658782+ChanTsune@users.noreply.github.com>
2020-07-12 11:08:03 +0900

    Update __init__.py

--- UTIR/__init__.py
+++ UTIR/__init__.py
@@ -20,21 +20,20 @@ class Code2IR:
     ir_ast_serializer_class = ASTSerializer
     ir_ast_dumper_class = YamlDumper
 
-    @classmethod
-    def convert(cls, read_path, write_path):
-        source_reader = cls.source_reader_class()
+    def convert(self, read_path, write_path):
+        source_reader = self.source_reader_class()
         python_ast = source_reader.readf(read_path)
-        for transformer_class in cls.py_ast_transformer_classes:
+        for transformer_class in self.py_ast_transformer_classes:
             transformer = transformer_class()
             python_ast = transformer.visit(python_ast)
-        ast_converter = cls.ast_converter_class()
+        ast_converter = self.ast_converter_class()
         ir_ast = ast_converter.convert(python_ast)
-        for transformer_class in cls.ir_ast_transformer_classes:
+        for transformer_class in self.ir_ast_transformer_classes:
             transformer = transformer_class()
             ir_ast = transformer.visit(ir_ast)
-        ast_serializer = cls.ir_ast_serializer_class()
+        ast_serializer = self.ir_ast_serializer_class()
         object = ast_serializer.serialize(ir_ast)
-        object_dumper = cls.ir_ast_dumper_class()
+        object_dumper = self.ir_ast_dumper_class()
         object_dumper.dumpf(write_path, object)
 
 
@@ -46,20 +45,19 @@ class IR2Code:
     py_ast_transformer_classes = []
     py_code_generator_class = PyASTToCodeGenerator
 
-    @classmethod
-    def convert(cls, read_path, write_path):
-        loader = cls.ir_ast_loader_class()
+    def convert(self, read_path, write_path):
+        loader = self.ir_ast_loader_class()
         object = loader.loadf(read_path)
-        deserializer = cls.ir_ast_deserializer_class()
+        deserializer = self.ir_ast_deserializer_class()
         ir_ast = deserializer.deserialize(object)
-        for transformer_class in cls.ir_ast_transformer_classes:
+        for transformer_class in self.ir_ast_transformer_classes:
             transformer = transformer_class()
             ir_ast = transformer.visit(ir_ast)
-        ast_converter = cls.ast_converter_class()
+        ast_converter = self.ast_converter_class()
         python_ast = ast_converter.convert(ir_ast)
-        for transformer_class in cls.py_ast_transformer_classes:
+        for transformer_class in self.py_ast_transformer_classes:
             transformer = transformer_class()
             python_ast = transformer.visit(python_ast)
-        code_generator = cls.py_code_generator_class()
+        code_generator = self.py_code_generator_class()
         code = code_generator.generate(python_ast)
         print(code)
