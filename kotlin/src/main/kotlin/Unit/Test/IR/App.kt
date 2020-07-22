a3b6d9487f981ef7ac420af6864fd54aab1eec53
taiki tsunekawa <41658782+ChanTsune@users.noreply.github.com>
2020-07-22 12:55:45 +0900

    Update App.kt

--- kotlin/src/main/kotlin/Unit/Test/IR/App.kt
+++ kotlin/src/main/kotlin/Unit/Test/IR/App.kt
@@ -6,19 +6,6 @@ package Unit.Test.IR
 import kastree.ast.Writer
 
 
-fun _main() {
-//    val scriptFile = "/Users/tsunekwataiki/Documents/GitHub/Unit-Test-IR/kotlin/src/main/kotlin/Unit/Test/IR/App.kt"
-//
-//    val parser = KotlinScriptParser()
-//
-//    val analyzeContext = parser.parse(scriptFile)
-//
-//    println(analyzeContext)
-//
-//    val function = analyzeContext.functions.keys.first()
-//    val body = function.bodyExpression as KtBlockExpression
-//    println(body)
-}
 
 
 fun main(args: Array<String>) {
@@ -33,9 +20,14 @@ fun main(args: Array<String>) {
         fun baz() = println("Hello, again!")
     """.trimIndent()
     // Call the parser with the code
-    val file = Parser.parseFile(code)
-    println(file)
-    println(file.decls)
+    try {
+        val file = Parser.parseFile(code)
+        println(file)
+        println(file.decls)
 
-    println(Writer.write(file))
+
+        println(Writer.write(file))
+    } catch (e: Parser.ParseError){
+        println(e)
+    }
 }
