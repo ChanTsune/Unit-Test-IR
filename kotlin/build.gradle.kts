ccffdd644cbcb08ec754fd68f55fbd1084b15ee7
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2020-09-14 21:53:00 +0900

    Update kotlin version

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -6,8 +6,7 @@
 
 plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.3.72"
-
+    id("org.jetbrains.kotlin.jvm") version "1.4.0"
     // Apply the application plugin to add support for building a CLI application.
     application
 }
@@ -21,9 +20,7 @@ repositories {
 dependencies {
     // Align versions of all Kotlin components
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-
-    // Use the Kotlin JDK 8 standard library.
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+//    implementation(platform("org.jetbrains.kotlinx:kotlinx-io:0.1.16"))
 
     // kotlin ast libraries
     // [kastree](https://github.com/cretz/kastree)
