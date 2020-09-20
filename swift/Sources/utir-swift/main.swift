f3d0cefe2f05db9472a87e3b17768020fb371c64
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2020-09-20 18:57:17 +0900

    Update main.swift

--- swift/Sources/utir-swift/main.swift
+++ swift/Sources/utir-swift/main.swift
@@ -5,33 +5,41 @@ import Foundation
 /// (if it is an integer literal token) add 1 to the integer and return the
 /// new integer literal token.
 class AddOneToIntegerLiterals: SyntaxRewriter {
-  override func visit(_ token: TokenSyntax) -> Syntax {
-    // Only transform integer literals.
-    guard case .integerLiteral(let text) = token.tokenKind else {
-      return Syntax(token)
+    override func visit(_ token: TokenSyntax) -> Syntax {
+        // Only transform integer literals.
+        guard case .integerLiteral(let text) = token.tokenKind else {
+            return Syntax(token)
+        }
+
+        // Remove underscores from the original text.
+        let integerText = String(text.filter { ("0"..."9").contains($0) })
+
+        // Parse out the integer.
+        let int = Int(integerText)!
+
+        // Create a new integer literal token with `int + 1` as its text.
+        let newIntegerLiteralToken = token.withKind(.integerLiteral("\(int + 1)"))
+
+        // Return the new integer literal.
+        return Syntax(newIntegerLiteralToken)
     }
+}
 
-    // Remove underscores from the original text.
-    let integerText = String(text.filter { ("0"..."9").contains($0) })
-
-    // Parse out the integer.
-    let int = Int(integerText)!
-
-    // Create a new integer literal token with `int + 1` as its text.
-    let newIntegerLiteralToken = token.withKind(.integerLiteral("\(int + 1)"))
-    
-    // Return the new integer literal.
-    return Syntax(newIntegerLiteralToken)
-  }
+class MyVisitor: SyntaxAnyVisitor {
+    override func visitAnyPost(_ node: Syntax) {
+        print(type(of: node))
+        print(node.customMirror)
+    }
 }
 
 
-func main(_ argv:[String]) {
+func main(_ argv: [String]) {
     do {
         print(FileManager.default.currentDirectoryPath)
-        let file = "./Sources/utir-swift/main.swift"
+        let file = "/Users/tsunekwataiki/Documents/GitHub/Unit-Test-IR/swift/Sources/utir-swift/main.swift"
         let url = URL(fileURLWithPath: file)
         let sourceFile = try SyntaxParser.parse(url)
+        MyVisitor().walk(sourceFile)
         let incremented = AddOneToIntegerLiterals().visit(sourceFile)
         print(incremented)
     } catch {
