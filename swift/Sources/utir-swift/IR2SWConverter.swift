e0775b3637dadf2fe6d84c85a3472584abbad69d
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2020-09-20 18:57:21 +0900

    Create IR2SWConverter.swift

---
+++ swift/Sources/utir-swift/IR2SWConverter.swift
@@ -0,0 +1,14 @@
+//
+//  IR2SWConverter.swift
+//  utir-swift
+//
+//  Created by tsunekwataiki on 2020/09/20.
+//
+
+import SwiftSyntax
+
+class IR2SWConverter {
+    func visit(_ node: Syntax) {
+
+    }
+}
