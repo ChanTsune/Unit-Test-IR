be524bfba4cf9130c82aa51ce2e80f1614503bf8
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2020-10-19 16:36:40 +0900

    Update main.ml

--- OCaml/bin/main.ml
+++ OCaml/bin/main.ml
@@ -1,7 +1,62 @@
 include Ast_type
 
-let parse y =
+
+let get_dict_node_param key y =
+  List.find (fun x -> let (k,_) = x in k = key) y
+
+let get_node_type y =
+let _, node_type = get_dict_node_param "Node" y in
+match node_type with
+| `String s -> s
+| _ -> raise Not_found
+
+let rec parse_node (y:Yaml.value) =
+  match y with
+  | `O o -> begin
+    let node_type = get_node_type o in
+    if node_type = "File" then
+      let _, v = get_dict_node_param "Version" o in
+      let v = match v with
+      | `Float f -> int_of_float f
+      | _ -> raise (Invalid_argument "Dose not match as file node") in
+      let _, b = get_dict_node_param "Body" o in
+      let c = match b with
+       | `A a -> List.map parse_decl a
+       | _ -> raise (Invalid_argument "Dose not match as file node") in
+      File {file_version = v;file_body = c}
+    else if node_type = "Block" then
+      let _, b = get_dict_node_param "Body" o in
+      let c = match b with
+      | `A a -> List.map parse_stmt a
+      | _ -> raise (Invalid_argument "Dose not match as file node") in
+      Block {block_body = c}
+    else
+      raise (Invalid_argument "Dose not match as file node")
+    end
+  | _ -> raise (Invalid_argument "Dose not match as file node")
+
+and parse_stmt y =
 match y with
+  | `O o -> begin
+    let node_type = get_node_type o in
+    if node_type = "Expr" then
+      let _,expr = get_dict_node_param "Expr" o in
+     ExprStmt {expr = parse_expr expr}
+    else
+     raise (Invalid_argument "Dose not match as stmt node")
+   end
+| _ -> raise (Invalid_argument "Dose not match as stmt node")
+
+and parse_decl y =
+match y with
+| _ -> raise (Invalid_argument "Dose not match as decl node")
+
+and parse_expr y =
+match y with
+| _ -> raise (Invalid_argument "Dose not match as expr node")
+
+let parse y =
+  match y with
   | `Null -> Constant { constant_kind = Null; constant_value = "null"}
   | `String s -> Constant {constant_kind = String; constant_value = s}
   | `Bool b -> Constant {constant_kind = Boolean; constant_value = string_of_bool b}
@@ -20,7 +75,7 @@ match y with
   let inputFile = Fpath.(v input) in
   let outputFile = Fpath.(v output) in
   let parsed = Yaml_unix.of_file_exn inputFile in
-  let a = parse parsed
+  let a = parse_node parsed
   in
   let _ = a in
   let () = Yaml.pp Format.std_formatter parsed in
