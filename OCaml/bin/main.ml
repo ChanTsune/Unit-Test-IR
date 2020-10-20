e74b5b93cafd158f74aebd2bc375ec185b6b87b1
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2020-10-20 12:02:41 +0900

    Update main.ml

--- OCaml/bin/main.ml
+++ OCaml/bin/main.ml
@@ -1,5 +1,12 @@
 include Ast_type
 
+exception TypeError
+
+let raise_ir_parse_error msg y =
+  let s = Yaml.to_string_exn y in
+  raise (Invalid_argument (msg ^ "\n" ^ s))
+
+let case_of_decl decl = match decl with| Case c -> c |_ -> raise TypeError
 
 let get_dict_node_param key y =
   List.find (fun x -> let (k,_) = x in k = key) y
@@ -25,35 +32,71 @@ let rec parse_node (y:Yaml.value) =
        | _ -> raise (Invalid_argument "Dose not match as file node") in
       File {file_version = v;file_body = c}
     else if node_type = "Block" then
-      let _, b = get_dict_node_param "Body" o in
-      let c = match b with
-      | `A a -> List.map parse_stmt a
-      | _ -> raise (Invalid_argument "Dose not match as file node") in
-      Block {block_body = c}
+      Block (parse_block o)
     else
-      raise (Invalid_argument "Dose not match as file node")
+      raise_ir_parse_error "Dose not match as file node" y
     end
-  | _ -> raise (Invalid_argument "Dose not match as file node")
+  | _ -> raise_ir_parse_error "Invalid Yaml.value passed! Dose not match as file node" y
 
+and parse_block o =
+  let c = match get_dict_node_param "Body" o with
+  | _, `A a -> a |> List.map parse_stmt
+  | _ -> raise (Invalid_argument "Dose not match as block node")
+  in {block_body = c}
 and parse_stmt y =
 match y with
   | `O o -> begin
     let node_type = get_node_type o in
-    if node_type = "Expr" then
+    match node_type with
+    | "Expr" ->
       let _,expr = get_dict_node_param "Expr" o in
-     ExprStmt {expr = parse_expr expr}
-    else
-     raise (Invalid_argument "Dose not match as stmt node")
+      ExprStmt {expr = parse_expr expr}
+    | "Decl" ->
+      let _, decl = get_dict_node_param "Decl" o in
+      DeclStmt {decl = parse_decl decl}
+    | _ -> raise_ir_parse_error "Dose not match as any stmt node" y
    end
-| _ -> raise (Invalid_argument "Dose not match as stmt node")
+| _ -> raise_ir_parse_error "Invalid Yaml.value passed! Dose not match as stmt node" y
 
 and parse_decl y =
 match y with
-| _ -> raise (Invalid_argument "Dose not match as decl node")
+  | `O o -> begin
+    let node_type = get_node_type o in
+    match node_type with
+    | "Suite" ->
+      let name = match get_dict_node_param "Name" o with
+       | _,`String s -> s
+       | _ -> raise TypeError in
+      let setup = match get_dict_node_param "SetUp" o with
+       | _,`A a -> List.map parse_expr a
+       | _ -> raise TypeError in
+      let cases = match get_dict_node_param "Cases" o with
+      | _, `A a -> a |> List.map parse_decl |> List.map case_of_decl
+      | _ -> raise TypeError in
+      let tear_down = match get_dict_node_param "TearDown" o with
+      | _, `A a -> List.map parse_expr a
+      | _ -> raise TypeError in
+      Suite {suite_name = name; suite_set_up = setup; suite_cases = cases; suite_tear_down = tear_down}
+    | "CaseBlock" ->
+      let name = match get_dict_node_param "Name" o with
+      | _, `String s -> s
+      | _ -> raise TypeError in
+      let block = match get_dict_node_param "Body" o with
+      | _, `O o -> parse_block o
+      |_ -> raise TypeError in
+      Case (CaseBlock {case_block_name=name;case_block_body=block})
+    | _ -> raise_ir_parse_error "Dose not match as any decl node" y
+   end
+| _ -> raise_ir_parse_error "Invalid Yaml.value passed! Dose not match as any decl node" y
 
 and parse_expr y =
 match y with
-| _ -> raise (Invalid_argument "Dose not match as expr node")
+  | `O o -> begin
+    let node_type = get_node_type o in
+    match node_type with
+    |_ -> raise_ir_parse_error "Dose not match as any expr node" y
+   end
+| _ -> raise_ir_parse_error "Invalid Yaml.value passed! Dose not match as any expr node" y
 
 let parse y =
   match y with
