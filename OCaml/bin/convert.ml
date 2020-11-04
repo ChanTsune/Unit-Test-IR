745ee1f1246d06d1a6bd2e5b17045cc55572875a
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2020-11-04 15:09:06 +0900

    Update convert.ml

--- OCaml/bin/convert.ml
+++ OCaml/bin/convert.ml
@@ -74,7 +74,20 @@ and block_node_to n =
   let to_seqence list =
     let rec iter list expr =
       match list with
-      |hd::tl -> make_expression (Pexp_sequence (expr, iter tl hd))
+      |hd::tl -> begin match expr with
+                      | {
+                        pexp_desc = Pexp_let (rec_flag , value_binding_list , {pexp_desc = Pexp_unreachable;pexp_loc = _;pexp_loc_stack = _;pexp_attributes = _;});
+                        pexp_loc = loc;
+                        pexp_loc_stack = loc_stack;
+                        pexp_attributes = attributes;
+                        } -> {
+                        pexp_desc = Pexp_let (rec_flag , value_binding_list , iter tl hd);
+                        pexp_loc = loc;
+                        pexp_loc_stack = loc_stack;
+                        pexp_attributes = attributes;
+                        }
+                      | _ -> make_expression (Pexp_sequence (expr, iter tl hd))
+                  end
       |[] -> expr
     in
     match list with
