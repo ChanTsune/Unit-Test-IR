f51fa55e44e8310c78217d2c05b870c1348938b6
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2020-11-06 14:58:53 +0900

    Update main.ml

--- OCaml/bin/main.ml
+++ OCaml/bin/main.ml
@@ -11,8 +11,9 @@ let convert_and_write input output =
   in
   let _ = a in let _ = outputFile in
   let ocaml_structures = Convert.structure_of_ir_node a in
-  let () = print_ast_as_code ocaml_structures in
-  let () = print_ast ocaml_structures in
+  (* let _ = Ast_invariants.structure ocaml_structures in *)
+  (* let () = print_ast_as_code ocaml_structures in *)
+  (* let () = print_ast ocaml_structures in *)
   let str = Ocaml_common.Pprintast.string_of_structure ocaml_structures in
   let _ = Bos.OS.File.write outputFile str in
   ()
@@ -31,4 +32,20 @@ let main argc argv =
 let () = main (Array.length Sys.argv) Sys.argv
 
 
+let main () =
+  let s = Compile_common.parse_impl {
+  source_file = "/Users/tsunekwataiki/Documents/GitHub/mlpystring/test/test.ml";
+  module_name = "My_Module";
+  output_prefix = "prefix_";
+  env = Env.empty;
+  ppf_dump = Format.std_formatter;
+  tool_name = "my_tool";
+  native = false;
+  }
+  in
+  (* Ocaml_common.Ast_helper.lid *)
+  let _ = print_ast s in
+  let _ = List.iter (fun _ -> ()) s in
+  print_ast_as_code s
 
+let _ = main ()
