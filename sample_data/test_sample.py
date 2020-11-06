bd45df8d4fbce4ef990b7c7fe976951b8ab08f81
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2020-11-06 14:58:39 +0900

    Update test_sample.py

--- sample_data/test_sample.py
+++ sample_data/test_sample.py
@@ -8,3 +8,11 @@ class IRSampleTest(TestCase):
         b = 2
         c = 3
         self.assertEqual(a + b, c)
+
+    def test_case_two(self):
+        a = 1
+        self.assertEqual(a, 1)
+
+    def test_case_3(self):
+        a = 1
+        self.assertEqual(a, 1)
